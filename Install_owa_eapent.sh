0424b1066d6fcc23bf0d4bc507746394744ed3c3
ShenWang9202 <45646455+ShenWang9202@users.noreply.github.com>
2020-08-08 15:08:37 -0500

    Update Install_owa_eapent.sh

--- Install_owa_eapent.sh
+++ Install_owa_eapent.sh
@@ -3,3 +3,19 @@ virtualenv -p /usr/bin/python3 virtualenvironment/project_1
 source virtualenvironment/project_1/bin/activate
 
 cd virtualenvironment/project_1/
+
+pip3 install scikit-build
+pip3 install pipenv
+
+git clone https://github.com/ShenWang9202/epanet-python.git
+
+
+cd epanet-python/
+git submodule update --init
+cd owa-epanet
+
+./scripts/clean.sh
+
+python3 setup.py sdist bdist_wheel
+
+cd test && pipenv install ../dist/*.whl && pipenv install pytest && pipenv run pytest
